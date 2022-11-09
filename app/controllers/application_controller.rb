87413ab3ec347f199a9df0dc5ab71b004e2cd41a
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-11-09 13:08:41 +0300

    minor changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,19 +7,13 @@ class ApplicationController < Sinatra::Base
     options "*" do
         response.headers["Access-Control-Allow-Methods"]="GET,POST,PUT,PATCH,DELETE,OPTIONS"
     end
-
-    # to get all buyers
+# BUYERS END-POINTS 
         get "/buyers" do
             Buyer.all.to_json
         end
-
-    # to get all sellers
         get "/sellers" do
             Seller.all.to_json
         end
-
-
-# POST IN buyers
         post "/buyers" do
             buyers=Buyer.create(
                 name: params[:name],
@@ -28,15 +22,13 @@ class ApplicationController < Sinatra::Base
             )
             buyers.to_json
         end
-
-# DELETE IN Buyers
         delete "/buyers/:id" do
             buyers = Buyer.find(params[:id])
             buyers.destroy
             {message: "Buyer '#{buyers.name}' has been deleted."}.to_json
         end
 
-# POST in sellers
+# SELLER'S END-POINTS
         post "/sellers" do
             sellers=Seller.create(
                 name: params[:name],
@@ -45,11 +37,41 @@ class ApplicationController < Sinatra::Base
             )
             sellers.to_json
         end
-# DELETE IN SELLERS
-    delete "/sellers/:id" do
-        sellers=Seller.find(params[:id])
-        sellers.destroy
-        {message: "seller '#{sellers.name}' has been deleted."}.to_json
-    end     
+        delete "/sellers/:id" do
+            sellers=Seller.find(params[:id])
+            sellers.destroy
+            {message: "seller '#{sellers.name}' has been deleted."}.to_json
+        end 
+    
+# PRODUCTS END-POINTS
+        get "/products" do
+            Product.all.to_json
+        end
+        post "/products" do
+            products=Product.create(
+                name: params[:name],
+                category: params[:category],
+                price: params[:price],
+                description: params[:description]
+            )
+            products.to_json
+        end
+        delete "/products/:id" do
+            Product.find(params[:id]).destroy
+        end
+# ORDERS ENDPOINTS
+        get "/orders" do
+            Order.all.to_json
+        end
+        post "/orders" do
+            Order.create(
+                buyer_id: params[:buyer_id],
+                product_id: params[:product_id],
+                total: params[:total]
+            ).to_json
+        end
+        delete "/orders/:id" do
+            Order.find(params[:id]).destroy
+        end
         
 end
