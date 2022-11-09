0597f580a971451f8800056b37221c36d0be365a
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-11-09 13:35:16 +0300

    initial code for the endpoints

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_08_072947) do
+ActiveRecord::Schema.define(version: 2022_11_09_095226) do
 
   create_table "buyers", force: :cascade do |t|
     t.string "name"
     t.text "email"
     t.text "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "buyer_id"
+    t.integer "product_id"
+    t.integer "total"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "category"
+    t.text "description"
+    t.text "image"
+    t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
